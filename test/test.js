ce60b746998791be4ee71b071f21ebf7f8d4d1fe
刘祺 <qil@jumei.com>
2017-06-13 17:21:23 +0800

    fix eslint & precommit

--- test/test.js
+++ test/test.js
@@ -1,49 +1,51 @@
+/* eslint-env node, mocha */
 /* eslint-disable no-console, func-names */
-'use strict';
+'use strict'
 
-require('should');
-var proxyquire = require('proxyquire');
-var fs = require('fs');
-var streamingParser = require('../lib/parser');
+require('should')
+var proxyquire = require('proxyquire')
+var fs = require('fs')
+var streamingParser = require('../lib/parser')
+var path = require('path')
 
-describe('git-blame', function() {
-  it('should parse the git-blame output', function(done) {
+describe('git-blame', function () {
+  it('should parse the git-blame output', function (done) {
     var output = {
       lines: [],
       commits: []
-    };
-    streamingParser(fs.createReadStream(__dirname + '/fixture.txt', 'utf8'))
-      .on('data', function(type, data) {
-        output[type + 's'].push(data);
-      }).on('error', function(err) {
-        throw err;
-      }).on('end', function() {
-        output.should.eql(require('./output.json'));
+    }
+    streamingParser(fs.createReadStream(path.join(__dirname, 'fixture.txt'), 'utf8'))
+      .on('data', function (type, data) {
+        output[type + 's'].push(data)
+      }).on('error', function (err) {
+        throw err
+      }).on('end', function () {
+        output.should.eql(require('./output.json'))
 
-        done();
-      });
-  });
+        done()
+      })
+  })
 
-  it('should delegate with the correct params', function(done) {
-    var repoPath = '/home/node.git';
-    var opts = { rev: 'master', file: 'CHANGES.md' };
+  it('should delegate with the correct params', function (done) {
+    var repoPath = '/home/node.git'
+    var opts = { rev: 'master', file: 'CHANGES.md' }
 
     var gitBlame = proxyquire.load('../', {
-      './lib/parser': function(inputStream) {
-        inputStream.should.eql('git-spawned-stream');
+      './lib/parser': function (inputStream) {
+        inputStream.should.eql('git-spawned-stream')
 
-        return 'streamingParser';
+        return 'streamingParser'
       },
-      'git-spawned-stream': function(path, args) {
-        path.should.eql(repoPath);
-        args.should.eql(['blame', opts.rev, '-p', '--', opts.file]);
+      'git-spawned-stream': function (path, args) {
+        path.should.eql(repoPath)
+        args.should.eql(['blame', opts.rev, '-p', '--', opts.file])
 
-        return 'git-spawned-stream';
+        return 'git-spawned-stream'
       }
-    });
+    })
 
-    gitBlame(repoPath, opts).should.eql('streamingParser');
+    gitBlame(repoPath, opts).should.eql('streamingParser')
 
-    done();
-  });
-});
+    done()
+  })
+})
